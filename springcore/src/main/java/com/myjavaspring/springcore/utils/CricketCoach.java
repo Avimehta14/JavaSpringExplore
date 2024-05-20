a7478839fc6e34bb1232fa7956d4a8afa109b47b
avimehta14 <avimehta14@gmail.com>
2024-05-19 22:33:33 -0500

    Pre and post annotations (Primary annotation)

--- springcore/src/main/java/com/myjavaspring/springcore/utils/CricketCoach.java
+++ springcore/src/main/java/com/myjavaspring/springcore/utils/CricketCoach.java
@@ -1,13 +1,29 @@
 package com.myjavaspring.springcore.utils;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class CricketCoach implements Coach{
 
     @Override
     public String getDailyWorkout()
     {
         return "Best cricket team is India";
+    }
+
+    @PostConstruct
+    public void afterConstruct()
+    {
+        System.out.println("Annotation to define any logic to run after bean construction");
+    }
+
+    @PreDestroy
+    public void beforeDestroy()
+    {
+        System.out.println("Annotation to define any logic to run before bean destruction");
     }
 }
